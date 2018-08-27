0426202414574f28c6e688c509227efb57680bca
Mike Bond <mikebond84@gmail.com>
2018-08-27 08:51:38 -0400

    commit before class, basic returning and some styling

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 import 'shoelace-css/dist/shoelace.css'
 import './text.css'
-
 import request from 'superagent'
 
-request.get('https://itunes.apple.com/search?term=""&entity=musicTrack')
-  .then(response => {
-    let results = response.body
-    console.log (response)
-  })
+getId('search-form').addEventListener('submit', (event) => {
+  event.preventDefault()
 
-function displayResults (results) {
-  let output = []
-  var array = output
+  let searchResults = getId('search-form')
+  searchResults.addEventListener('click', function (event) {
+  })
+  let searchField = getId('search')
+  let searchTerm = window.encodeURIComponent(searchField.value)
+  request.get(`https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=song&attribute=artistTerm`)
+    .then(response => {
+      return JSON.parse(response.text)
+    })
+    .then(body => {
+      let searchResults = body.results
+      for (var i = 0; i < body.results.length; i++) {
+        let output = getId('output')
+        let outputLi = document.createElement('li')
+        outputLi.innerText = searchResults[i].trackName
+        output.appendChild(outputLi)
+      }
+    })
+})
+
+function getId (id) {
+  return document.getElementById(id)
 }
-//   array.forEach(function() {
-//   document.getElementById("search").innerHTML = array
-//   console.log(results)
-// })
